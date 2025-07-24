77fda88f76eb5f6a66526bff269235d08a9ae7e7
ibrahim-ahmed201 <ibrahimahmed384888@gmail.com>
2025-07-24 21:12:39 +0300

    Finsihed Section

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,20 +1,40 @@
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 const Calculator = memo(function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0);
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  // const totalDuration = duration + durationCounter;
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  useEffect(
+    function () {
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    },
+    [number, sets, speed, durationBreak]
+  );
+  useEffect(
+    function () {
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+      playSound();
+    },
+    [allowSound, duration]
+  );
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
 
+  function increment() {
+    setDuration((prev) => Math.floor(Number(prev + 1)));
+  }
+  function decerment() {
+    setDuration((prev) => (prev > 1 ? Math.ceil(prev - 1) : 0));
+  }
   return (
     <>
       <form>
@@ -64,13 +84,13 @@ const Calculator = memo(function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={decerment}>–</button>
         <p>
           {mins < 10 && "0"}
           {mins}:{seconds < 10 && "0"}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={increment}>+</button>
       </section>
     </>
   );
